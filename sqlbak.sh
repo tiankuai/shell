bba315307055a485aec0c39d7ac6369f601d732b
“tiankuai” <864127133@qq.com>
2018-07-19 16:24:03 +0800

    sql

--- sqlbak.sh
+++ sqlbak.sh
@@ -6,26 +6,35 @@ set -e
 #PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:/usr/local/mysql/bin
 #export PATH
 #数据库地址
-dbip='114.215.17'
+dbip='rm-m5e286yn'
 #数据库用户名
-dbuser='testdb'
+dbuser='haitong'
 #数据库密码
-dbpasswd='123456'
+dbpasswd='haitong'
 #数据库名,可以定义多个数据库，中间以空格隔开，如：test test1 test2
-dbname='dpop_sso dpop_bgcz dpop_ipo'
+dbname='_ht'
+#DAYS=15代表删除15天前的备份，即只保留近15天的备份
+DAYS=7
 #备份时间
 backtime=`date +%Y%m%d%H%M%S`
 #日志备份路径
-logpath='/home/tiank/logbackup'
+logpath='/home/tiank/mysqlbackup/myqlbak.log'
 #数据备份路径
-datapath='/home/tiank/logbackup'
+datapath='/home/tiank/mysqlbackup'
+#create backup dir，校验是否存在backup目录
+if [ ! -d "$datapath" ]; then
+    mkdir -p "$datapath"
+fi
+if [ ! -d "$logpath" ]; then
+    touch "$logpath"
+fi
 #日志记录头部
-echo ‘”备份时间为${backtime},备份数据库表 ${dbname} 开始” >> ${logpath}/mysqllog.log
+echo ‘”备份时间为${backtime},备份数据库表 ${dbname} 开始” >> ${logpath}
 #正式备份数据库
 for table in $dbname; do
 source=`mysqldump -h${dbip} -u${dbuser} -p${dbpasswd} ${table} | gzip > ${datapath}/${table}${backtime}.sql.gz`
 #删掉之前的
-delete=`find ${datapath} -mtime +30 -name "*.gz" -exec rm -rf {} \;` 
+delete=`find ${datapath} -mtime +${DAYS} -name "*.gz" -exec rm -rf {} \;` 
 #备份成功以下操作
 if [ "$?" == 0 ];then
 #cd $datapath
@@ -33,16 +42,10 @@ if [ "$?" == 0 ];then
 #tar jcf ${table}${backtime}.tar.bz2 ${backtime}.sql > /dev/null
 #删除原始文件，只留压缩后文件
 #rm -f ${datapath}/${backtime}.sql
-echo “数据库表 ${dbname} 备份成功!!” >> ${logpath}/mysqllog.log
+echo “数据库表 ${table} 备份成功!!” >> ${logpath}
 else
 #备份失败则进行以下操作
-echo “数据库表 ${dbname} 备份失败!!” >> ${logpath}/mysqllog.log
+echo “数据库表 ${table} 备份失败!!” >> ${logpath}
 fi
 done
 
-#mysqldump -uroot -pXXXXX  --databases ttal  --master-data | gzip > /tmp/pri20180129.sql.gz
-
-
-#gunzip pri20180129.sql.gz
-
-#mysql  -uroot -pXXXX ttal < /tmp/pri20180129.sql
